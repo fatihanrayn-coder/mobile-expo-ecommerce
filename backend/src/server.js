4a14eaef2219d6cd4f63069042a8bfc11a6ab5a6
rayan <raynfatihan@gmail>
2026-02-09 08:42:17 +0300

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import express from 'express'; 
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "API is healthy" });
 });
 
-app.listen(3000, () => console.log('Server is ready running on port 3000'));
+//make our app ready for deployment
+
+if(ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../admin/dist'))); 
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../admin','dist','index.html'));
+    });  
+}
+
+app.listen(ENV.PORT, () => console.log('Server is ready running on port 3000'));
 
